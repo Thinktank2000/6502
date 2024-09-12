32d43fd786c26c1daf95e51d629eff95a399d78e
George <georgeskyrim@gmail.com>
2024-09-13 00:02:15 +0100

    implemented some instructions and refactored some code :)

--- Instructions.cs
+++ Instructions.cs
@@ -2,21 +2,49 @@
 {
     public class Instructions
     {
-        //debug method for INC instruction
-        public void IncrementMemory(Memory memory, Registers registers)
+        //method for INX instruction
+        public void INX(Registers registers)
         {
-            registers.PC += 2;
-            memory.memory[registers.PC] += 1;
-            CPU.clock += 5;
+            registers.PC += 1;
+            registers.X++;
+            registers.Clock += 2;
+
+            if (registers.X < 0)
+            {
+                registers.N = true;
+            }
+
+            else if (registers.X == 0)
+            {
+                registers.Z = true;
+            }
         }
 
-        //debug method for PHA instruction (push A to stack)
-        public void PushToStack(Memory memory, Registers registers)
+        //method for PHA instruction (push A to stack)
+        public void PHA(Memory memory, Registers registers)
         {
-            memory.memory[registers.SP] = registers.A;
             registers.SP += 1;
+            memory.memory[registers.SP] = registers.A;
             registers.PC += 1;
-            CPU.clock += 3;
+            registers.Clock += 3;
+
+            if (registers.SP > 0)
+            {
+                registers.N = true;
+            }
+
+            else if (registers.SP == 0)
+            {
+                registers.Z = true;
+            }
+        }
+
+        public void PLA(Memory memory, Registers registers)
+        {
+            registers.A = (byte)memory.memory[registers.SP];
+            registers.SP--;
+            registers.PC++;
+            registers.Clock += 4;
 
             if (registers.SP > 0)
             {
@@ -32,6 +60,54 @@
         public void ReadMemory(Memory memory, Registers registers, int memoryAddress)
         {
             Console.WriteLine("Value at Memory address: 0x{0:X} = {1}", memoryAddress, memory.memory[memoryAddress]);
+        }
+
+        public void ReadRegister(Registers registers, string register)
+        {
+           switch(register)
+            {
+                case "A":
+                    Console.WriteLine("The value in the register: A is {0}", registers.A);
+                    break;
+
+                case "X":
+                    Console.WriteLine("The value in the register: X is {0}", registers.X);
+                    break;
+
+                case "Y":
+                    Console.WriteLine("The value in the register: Y is {0}", registers.Y);
+                    break;
+
+                case "PC":
+                    Console.WriteLine("The location of the Program Counter is {0:X}", registers.PC);
+                    break;
+
+                case "SP":
+                    Console.WriteLine("The location of the Stack Pointer is {0:X}", registers.SP);
+                    break;
+
+                default:
+                    Console.WriteLine("Register not recognized");
+                    break;
+            }
+        }
+
+        public void ExecuteOpCode(byte opcode, Memory memory, Registers registers)
+        {
+            switch (opcode)
+            {
+                case 0xE8:
+                    INX(registers);
+                    break;
+
+                case 0x48:
+                    PHA(memory, registers);
+                    break;
+
+                case 0x68:
+                    PLA(memory, registers);
+                    break;
+            }
         }
     }
 }
--- Registers.cs
+++ Registers.cs
@@ -3,21 +3,23 @@
     public class Registers
     {
         //general purpose registers
-        public byte A;     //A register
-        public byte X;     //X register
-        public byte Y;     //Y register
+        public byte A { get; set; } //A register
+        public byte X { get; set; } //X register
+        public byte Y { get; set; } //Y register
 
         //special registers
-        public UInt16 PC;  //Program Counter
-        public UInt16 SP;  //Stack Pointer
+        public UInt16 PC { get; set; }  //Program Counter
+        public UInt16 SP { get; set; }  //Stack Pointer
 
         //processor status flags
-        public bool N;     //negative flag
-        public bool V;     //Overflow flag
-        public bool B;     //Break flag
-        public bool D;     //Decimal enable flag
-        public bool I;     //Interrupt disable flag
-        public bool Z;     //Zero flag
-        public bool C;     //Carry flag
+        public bool N { get; set; } //negative flag
+        public bool V { get; set; } //Overflow flag
+        public bool B { get; set; } //Break flag
+        public bool D { get; set; } //Decimal enable flag
+        public bool I { get; set; } //Interrupt disable flag
+        public bool Z { get; set; } //Zero flag
+        public bool C { get; set; } //Carry flag
+
+        public int Clock { get; set; } //CPU clock (1MHz)
     }
 }
